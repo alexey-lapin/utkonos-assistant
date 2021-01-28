b75848135ff490a071b87e864da2360890ec3f4b
Alexey Lapin <alexey-lapin@protonmail.com>
2021-01-28 22:14:17 +0300

    implement

--- src/price/index.js
+++ src/price/index.js
@@ -1,16 +1,29 @@
-const pattern1 = /\s+([0-9.]+)\s*(г|кг)$/;
+const patternItem = /\s+([0-9.]+)\s*(г|кг|мл|л)$/;
+const patternPack = /\s*(\d+)\s*(штук(?:и)? по)\s*([0-9.]+)\s*(г|кг|мл|л)$/;
 
 export function isPriceEligeableForCalculation(priceText) {
     return !priceText.includes("/");
 }
 
 export function isNameEligeableForCalculation(nameText) {
-    let newLocal = pattern1.exec(nameText);
-    if (newLocal) {
-        return new Descriptor(newLocal[1], newLocal[2]);
+    let packMatch = patternPack.exec(nameText);
+    if (packMatch) {
+        let count = parseInt(packMatch[1].trim());
+        return new Descriptor(handleQuantity(packMatch[3]) * count, packMatch[4]);
     }
-    console.log(">> r " + newLocal);
+    let itemMatch = patternItem.exec(nameText);
+    if (itemMatch) {
+        return new Descriptor(handleQuantity(itemMatch[1]), itemMatch[2]);
+    }
     return null;
+}
+
+function handleQuantity(quantity) {
+    if (quantity.startsWith("0.")) {
+        return quantity;
+    } else {
+        return quantity.replace(".", "");
+    }
 }
 
 export class Descriptor {
--- src/product/productCard.js
+++ src/product/productCard.js
@@ -1,19 +1,26 @@
-import  {isNameEligeableForCalculation, isPriceEligeableForCalculation} from "../price";
-
-// export function matches(element) {
-    // const found = element.querySelector("product-card");
-    // return found;
-// }
+import { isNameEligeableForCalculation, isPriceEligeableForCalculation } from "../price";
 
 function getProductName(element) {
-    // console.log(container.innerHTML);
     const newLocal = element.querySelector("utk-product-card-name span");
-    // console.log(">> product " + newLocal);
     return newLocal.textContent.trim();
 }
 
 function getProductPrice(element) {
     return element.querySelector("utk-product-price .product-price").textContent.trim();
+}
+
+function setPricePerUnit(element, price) {
+    let basePriceContainer = element.querySelector(".product-base-price");
+    let productOldPrice = basePriceContainer.querySelector(".product-old-price");
+    let pp = document.createElement("span");
+    pp.textContent = price;
+    if (productOldPrice) {
+        pp.style.marginRight = "5px";
+        productOldPrice.prepend(pp);
+    } else {
+        pp.style.marginTop = "-14px";
+        basePriceContainer.append(pp);
+    }
 }
 
 export class ProductCardHandler {
@@ -27,9 +34,13 @@ export class ProductCardHandler {
         let priceText = getProductPrice(element);
         let nameText = getProductName(element);
         let isPriceEligiable = isPriceEligeableForCalculation(priceText);
-        let n = isNameEligeableForCalculation(nameText);
-        if (isPriceEligiable && n) {
-            console.log(nameText + " | " + priceText + " | " + n.quantity);
+        let unit = isNameEligeableForCalculation(nameText);
+        if (isPriceEligiable && unit) {
+            console.log(nameText + " | " + priceText + " | " + unit.quantity);
+            let price = parseFloat(priceText.split(/\s+/)[0].replace(",", "."));
+            let pricePerUnit = price / unit.normalizedQuantity();
+            let pricePerUnitText = pricePerUnit.toFixed(2) + " ₽/" + unit.normalizedUnit();
+            setPricePerUnit(element, pricePerUnitText);
         }
     }
 
