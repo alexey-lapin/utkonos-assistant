e8b045a2e6304e5c9a0cce7d1504f362d642e840
Alexey Lapin <alexey-lapin@protonmail.com>
2021-01-30 23:15:15 +0300

    polish extension stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,53 @@
-const path = require('path');
-// import path from 'path';
-const CopyPlugin = require('copy-webpack-plugin');
-const ProgressBarPlugin = require('progress-bar-webpack-plugin');
-// import ProgressBarPlugin from 'progress-bar-webpack-plugin';
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
+const packageJson = require("./package.json");
 
 module.exports = {
-    devtool: 'eval-cheap-module-source-map',
-    context: __dirname + '/src',
-    entry: {
-        'inject/index': './inject/index.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-        alias: {
-          '@utkonos-web-ext/css': path.resolve(__dirname, './src/css/index'),
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-                options: {
-                  transpileOnly: true
-                },
-              },
-        ]
-    },
-    plugins: [
-        new ProgressBarPlugin(),
-        new CopyPlugin({
-            patterns: [{
-                from: 'extension/manifest.json',
-                to: 'manifest.json',
-            }]
-        }),
-    ]
+  devtool: "eval-cheap-module-source-map",
+  context: __dirname + "/src",
+  entry: {
+    "inject/index": "./inject/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ProgressBarPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "extension/img",
+          to: "img",
+        },
+        {
+          from: "extension/manifest.json",
+          to: "manifest.json",
+          transform: (content) => {
+            const jsonContent = JSON.parse(content);
+            jsonContent.version = packageJson.version;
+            jsonContent.description = packageJson.description;
+            jsonContent.homepage_url = packageJson.homepage;
+            return JSON.stringify(jsonContent, null, 2);
+          },
+        },
+      ],
+    }),
+  ],
 };
